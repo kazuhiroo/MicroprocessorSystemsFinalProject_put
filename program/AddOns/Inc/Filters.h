7ad80b422cc6956cbb75f3196148092684b3e596
kazuhiroo <katowski.antoni@gmail.com>
2026-01-17 17:21:50 +0100

    added moving average filter

--- program/AddOns/Inc/Filters.h
+++ program/AddOns/Inc/Filters.h
@@ -2,13 +2,20 @@
   * @file    : Filters.h
   * @author  : a.katowski, https://github.com/kazuhiroo
   * @date    : Jan 17, 2026
-  * @brief   : Implemented filters for the singals in the loop
+  * @brief   : Implemented filters for the signals in the loop
   *
 */
 
 #ifndef INC_FILTERS_H_
 #define INC_FILTERS_H_
 
+#define AVG_FILT_SQ 50
 
+
+/*
+ * @brief   : moving average filter defined by the samples quantity
+ *
+ * */
+float AvgFilter(float input);
 
 #endif /* INC_FILTERS_H_ */
--- program/AddOns/Src/Filters.c
+++ program/AddOns/Src/Filters.c
@@ -2,8 +2,32 @@
   * @file    : Filters.c
   * @author  : a.katowski, https://github.com/kazuhiroo
   * @date    : Jan 17, 2026
-  * @brief   : Implemented filters for the singals in the loop
+  * @brief   : Implemented filters for the signals in the loop
   *
 */
 
 #include "Filters.h"
+
+/*
+ * @brief   : moving average filter defined by the samples quantity
+ *
+ * */
+float AvgFilter(float input){
+	static float input_buffer[AVG_FILT_SQ] = {0};
+	float output = 0.0f;
+	float sum = 0.0f;
+
+	for(int i = 0; i < AVG_FILT_SQ; i++){
+		input_buffer[i] = input_buffer[i+1];
+	}
+
+	input_buffer[AVG_FILT_SQ-1] = input;
+
+	for(int i = 0; i < AVG_FILT_SQ; i++){
+		sum += input_buffer[i];
+	}
+
+	output = sum / AVG_FILT_SQ;
+
+	return output;
+}
