50695d30b088439f1b3ab6becec192422c2ccef0
kazuhiroo <katowski.antoni@gmail.com>
2026-01-25 15:16:54 +0100

    changed MATLAB tracking script to handle all of the control system signals

--- serial_port_test.m
+++ serial_port_test.m
@@ -3,22 +3,23 @@
 % * @date    : Jan 24, 2026
 % * @brief   : Tracking the response and data received from UART via MATLAB
 %              script
-% 
-
 
 s = serialport("COM3", 115200);  % set UART and baudrate
 configureTerminator(s,"CR/LF");  
 flush(s);
 
 N = 10000;           % samples quantity
-y = zeros(N,1);
+data = zeros(N,3);
 
 for k = 1:N
     line = readline(s);
-    y(k) = str2double(line);
+    rx = str2double(line);
+    data(k,:) = rx;
 end
 
-plot(y);
+subplot(3,1,1); plot(data(:,1), 'b'); title('y_ref');
+subplot(3,1,2); plot(data(:,2), 'r'); title('y');
+subplot(3,1,3); plot(data(:,3), 'y'); title('u');
 
 fclose(s);     % closing port
 delete(s);     % delete from memory
